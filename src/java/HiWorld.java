d18c92b0274b8952a98171598eec8413f1b3b0c3
Nelson-Gu <vincent950406@sina.com>
2021-05-10 10:35:20 +0800

    test

--- src/java/HiWorld.java
+++ src/java/HiWorld.java
@@ -1,12 +1,7 @@
 package java;
 
-/**
- * @author Nelson_Gu
- * @version 1.0
- * @date 2021/5/10 9:49
- */
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("hi world");
+        System.out.println("hii world");
     }
 }
