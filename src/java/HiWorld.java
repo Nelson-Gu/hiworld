949871841b5ac8eaddd28db94cbef07ff963384c
Nelson-Gu <vincent950406@sina.com>
2021-05-10 11:05:21 +0800

    test

--- src/java/HiWorld.java
+++ src/java/HiWorld.java
@@ -3,6 +3,6 @@ package java;
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
-        System.out.println("update");
+        System.out.println("update1");
     }
 }
